b44631f26d7ae37ca447ed2ef98b64bc5133c785
MarkBebrov <pro100nemo666@gmail.com>
2023-05-12 22:40:16 +0300

    YYYYYYYYYYYYYYYYYYYYYYYYYYY

---
+++ src/App.js
@@ -0,0 +1,291 @@
+import { React, useState, useEffect, useCallback } from 'react';
+import '@vkontakte/vkui/dist/vkui.css';
+import {
+	Panel,
+	PanelHeader,
+	Placeholder,
+	Button,
+	ModalRoot,
+	FixedLayout,
+	PanelHeaderClose,
+	Avatar,
+	Group,
+	Cell,
+	Title,
+	Text,
+	PanelHeaderBack,
+} from '@vkontakte/vkui';
+import { Icon28UserAddOutline } from '@vkontakte/icons';
+import Icon24Add from '@vkontakte/icons/dist/24/add';
+import QueueModal from './QueueModal';
+import QueueMenu from './QueueMenu';
+
+function App() {
+	const [prevPanel, setPrevPanel] = useState(null);
+
+	const [activePanel, setActivePanel] = useState("main");
+	const [currentUserId, setCurrentUserId] = useState(null);
+	const goToQueueMenu = () => {
+		setPrevPanel(activePanel);
+		setActivePanel("queueMenu");
+	  };
+	  
+	  const goBack = () => {
+		setActivePanel(prevPanel);
+	  };
+	  
+
+	const [modalVisible, setModalVisible] = useState(false);
+	const [queues, setQueues] = useState([]);
+
+	const [currentQueue, setCurrentQueue] = useState(null);
+
+
+
+	const handleQueueClick = (queue) => {
+		setCurrentQueue(queue);
+		goToQueueMenu();
+	};
+
+
+	const openModal = () => {
+		setModalVisible(true);
+	};
+
+	const closeModal = () => {
+		setModalVisible(false);
+	};
+
+	const addQueue = (newQueue) => {
+		setQueues((prevQueues) => [
+			...prevQueues,
+			{
+				...newQueue,
+				creatorId: currentUserId,
+			},
+		]);
+	};
+	const [timeUpdate, setTimeUpdate] = useState(0);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setTimeUpdate((prevTime) => prevTime + 1);
+		}, 1000);
+
+		return () => clearInterval(interval);
+	}, []);
+
+
+	const getTimeRemaining = (endDate) => {
+		const total = Date.parse(endDate) - Date.parse(new Date());
+		const seconds = Math.floor((total / 1000) % 60);
+		const minutes = Math.floor((total / 1000 / 60) % 60);
+		const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+		const days = Math.floor(total / (1000 * 60 * 60 * 24));
+
+		return {
+			total,
+			days,
+			hours,
+			minutes,
+			seconds,
+		};
+	};
+	const renderActiveQueues = () => {
+		const activeQueues = queues.filter(
+			(queue) => new Date() >= new Date(queue.startDate)
+		);
+
+		if (activeQueues.length === 0) return null;
+
+		return (
+			<Group header={<Title level="3" weight="semibold">Активные очереди</Title>}>
+
+				{activeQueues.map((queue, index) => {
+					const timeRemaining = getTimeRemaining(queue.endDate);
+					return (
+						<Cell
+							key={index}
+							before={<Avatar src={queue.creatorAvatar} />}
+							style={{
+								backgroundColor: '#FFFFFF',
+								borderRadius: 8,
+								marginBottom: 16,
+								marginLeft: 13,
+								marginRight: 13,
+								padding: 16,
+							}}
+							onClick={() => handleQueueClick(queue)}
+						>
+							<Title level="3" weight="semibold">
+								{queue.title}
+							</Title>
+							<Text style={{ fontSize: '15px', color: 'var(--text_secondary)' }} weight="regular">
+								Осталось времени: {timeRemaining.days}д {timeRemaining.hours}ч {timeRemaining.minutes}м {timeRemaining.seconds}с
+							</Text>
+
+
+						</Cell>
+					);
+				})}
+			</Group>
+		);
+	};
+
+
+	const renderUpcomingQueues = () => {
+		const upcomingQueues = queues.filter(
+			(queue) => new Date() < new Date(queue.startDate)
+		);
+
+		if (upcomingQueues.length === 0) return null;
+
+		return (
+			<Group header={<Title level="3" weight="semibold">Грядущие очереди</Title>}>
+				{upcomingQueues.map((queue, index) => {
+					const timeRemaining = getTimeRemaining(queue.startDate);
+					return (
+						<Cell
+							key={index}
+							before={<Avatar src={queue.creatorAvatar} />}
+							style={{
+								backgroundColor: '#FFFFFF',
+								borderRadius: 8,
+								marginBottom: 16,
+								marginLeft: 13,
+								marginRight: 13,
+								padding: 16,
+							}}
+							onClick={() => handleQueueClick(queue)}
+						>
+							<Title level="3" weight="semibold">
+								{queue.title}
+							</Title>
+							<Text style={{ fontSize: '15px', color: 'var(--text_secondary)' }} weight="regular">
+								До начала: {timeRemaining.days}д {timeRemaining.hours}ч {timeRemaining.minutes}м {timeRemaining.seconds}с
+							</Text>
+
+						</Cell>
+					);
+				})}
+			</Group>
+		);
+	};
+
+	useEffect(() => {
+		// Вызывайте функции для отрисовки очередей при обновлении timeUpdate
+		renderActiveQueues();
+		renderUpcomingQueues();
+	}, [queues, timeUpdate]);
+
+	useEffect(() => {
+		// Здесь вы должны получить идентификатор текущего пользователя (например, из API)
+		const userId = Math.floor(Math.random() * 100000); // временное решение для генерации случайного числа
+		setCurrentUserId(userId);
+	}, []);
+
+
+	const modal = (
+		<ModalRoot
+			activeModal={modalVisible ? 'createQueue' : null}
+			onClose={closeModal}
+		>
+			<QueueModal id="createQueue" onClose={closeModal} addQueue={addQueue} />
+		</ModalRoot>
+	);
+
+
+	const isUserCreator = (queue) => {
+		return queue.creatorId === currentUserId;
+	};
+
+
+	return (
+		<div style={{ background: '#EBEDF0', height: '100vh' }}>
+			{activePanel === "main" ? (
+				<Panel id="panel1">
+					<PanelHeader before={<PanelHeaderClose />} after={<Avatar size={30} />}>
+						Вочередь!
+					</PanelHeader>
+					<div
+						style={{
+							height: 'calc(100vh - 128px)',
+							display: 'flex',
+							flexDirection: 'column',
+							alignItems: 'stretch',
+						}}
+					>
+						{queues.length === 0 && (
+							<Placeholder
+								icon={<Icon28UserAddOutline width={56} height={56} />}
+								header="Очереди не найдены"
+							>
+								У вас пока нет ни одной очереди. Создайте первую.
+							</Placeholder>
+						)}
+						{renderActiveQueues()}
+						{renderUpcomingQueues()}
+					</div>
+					<div style={{ zIndex: 10 }}>
+						{modal}
+					</div>
+					<FixedLayout vertical="bottom">
+						<div
+							style={{
+								background: '#FFFFFF',
+								height: 72,
+								borderTopLeftRadius: 12,
+								borderTopRightRadius: 12,
+							}}
+						>
+							<Button
+								mode="commerce"
+								size="xl"
+								style={{
+									marginLeft: 16,
+									marginRight: 16,
+									marginTop: 12,
+									marginBottom: 12,
+									background: '#2688EB',
+									borderRadius: 12,
+									color: '#FFFFFF',
+									padding: '12px 16px',
+									width: 'calc(100% - 32px)',
+									boxShadow: 'none',
+									transition: 'box-shadow 0.15s ease-in-out',
+								}}
+								before={<Icon24Add fill="#ffffff" />}
+								onClick={openModal}
+								hoverMode="opacity"
+								activeMode="opacity"
+								className="queue-create-button"
+							>
+								Создать очередь
+							</Button>
+						</div>
+					</FixedLayout>
+				</Panel>
+			) : (
+				<>
+					<QueueMenu
+						id="queueMenu"
+						goBack={goBack}
+						queue={currentQueue}
+						isUserCreator={() => isUserCreator(currentQueue)}
+						props={{
+							queueTitle: currentQueue.title,
+							queueTimeInfo:
+								new Date() < new Date(currentQueue.startDate)
+									? `До начала: ${getTimeRemaining(currentQueue.startDate).days}д ${getTimeRemaining(currentQueue.startDate).hours}ч ${getTimeRemaining(currentQueue.startDate).minutes}м ${getTimeRemaining(currentQueue.startDate).seconds}с`
+									: `Осталось времени: ${getTimeRemaining(currentQueue.endDate).days}д ${getTimeRemaining(currentQueue.endDate).hours}ч ${getTimeRemaining(currentQueue.endDate).minutes}м ${getTimeRemaining(currentQueue.endDate).seconds}с`
+						}}
+					/>
+
+
+				</>
+			)}
+		</div>
+	);
+}
+
+export default App;
---
+++ src/QueueModal.js
@@ -0,0 +1,251 @@
+import React, { useState, useEffect } from 'react';
+
+import {
+    ModalPage,
+    ModalPageHeader,
+    PanelHeaderClose,
+    FormItem,
+    Subhead,
+    Tabs,
+    TabsItem,
+    Separator,
+    Input,
+    Button,
+    Group,
+    PanelHeaderContent,
+    Checkbox,
+    LocaleProvider,
+    DateInput,
+    Slider,
+    Select,
+    SegmentedControl,
+} from '@vkontakte/vkui';
+import { Icon28CancelOutline, Icon24ChevronDown } from '@vkontakte/icons';
+
+
+
+const QueueModal = ({ id, onClose, addQueue }) => {
+    const [activeTab, setActiveTab] = useState('обычные');
+    const [checkbox1, setCheckbox1] = useState(false);
+    const [checkbox2, setCheckbox2] = useState(false);
+    const [checkbox3, setCheckbox3] = useState(false);
+
+    const [queueName, setQueueName] = useState('');
+    const [nameStatus, setNameStatus] = useState('valid');
+    const [nameError, setNameError] = useState('');
+
+    const [startDate, setStartDate] = useState(undefined);
+    const [endDate, setEndDate] = useState(undefined);
+    const [dateError, setDateError] = useState('');
+
+    const [value, setValue] = useState(undefined);
+    const [enableTime, setEnableTime] = useState(true);
+    const [disablePast, setDisablePast] = useState(true);
+    const [disableFuture, setDisableFuture] = useState(false);
+    const [disablePickers, setDisablePickers] = useState(false);
+    const [closeOnChange, setCloseOnChange] = useState(true);
+    const [showNeighboringMonth, setShowNeighboringMonth] = useState(false);
+    const [disableCalendar, setDisableCalendar] = useState(false);
+    const [locale, setLocale] = useState('ru');
+    const [value2, setValue2] = useState(undefined);
+
+    const [submitClicked, setSubmitClicked] = useState(false);
+
+    useEffect(() => {
+        if (submitClicked) {
+            if (!queueName) {
+                setNameStatus('error');
+                setNameError('Это поле обязательно для заполнения');
+            } else if (queueName.length > 40) {
+                setNameStatus('error');
+                setNameError('Название не может быть длиннее 40 символов');
+            } else {
+                setNameStatus('valid');
+                setNameError('');
+            }
+        }
+    }, [queueName, submitClicked]);
+
+    useEffect(() => {
+        if (startDate && endDate && endDate < startDate) {
+            setDateError('Вы выбрали невозможную дату');
+        } else {
+            setDateError('');
+        }
+    }, [startDate, endDate]);
+
+    const handleSubmit = () => {
+        setSubmitClicked(true);
+        if (nameStatus === 'valid' && !dateError) {
+            // Создание новой очереди
+            const newQueue = {
+                id: Date.now(),
+                title: queueName,
+                startDate,
+                endDate,
+                activeTab,
+                checkbox1,
+                checkbox2,
+            };
+            addQueue(newQueue); // Используйте функцию addQueue здесь
+            onClose();
+        }
+    };
+    return (
+        <ModalPage
+            id={id}
+            header={
+                <ModalPageHeader
+                    left={<PanelHeaderClose onClick={onClose} aria-label="Закрыть" />}
+                >
+                    <PanelHeaderContent>Создание очереди</PanelHeaderContent>
+                </ModalPageHeader>
+            }
+            style={{ width: '100%', alignItems: 'center' }}
+        >
+            <FormItem top="Настройки очереди">
+                <SegmentedControl
+                    value={activeTab}
+                    onChange={(value) => setActiveTab(value)}
+                    options={[
+                        {
+                            label: 'Обычные',
+                            value: 'обычные',
+                        },
+                        {
+                            label: 'Дополнительные',
+                            value: 'дополнительные',
+                        },
+                    ]}
+                />
+            </FormItem>
+
+
+            <Separator style={{ margin: '12px 0' }} />
+
+            {activeTab === 'обычные' && (
+                <>
+                    <FormItem
+                        top="Название"
+                        status={nameStatus}
+                        bottom={nameError}
+                    >
+                        <Input
+                            placeholder="Введите название"
+                            value={queueName}
+                            onChange={(e) => setQueueName(e.target.value)}
+                        />
+                    </FormItem>
+
+                    <FormItem top="Дополнительные настройки">
+                        <Checkbox checked={checkbox1} onChange={() => setCheckbox1(!checkbox1)}>
+                            Ограничить количество человек
+                        </Checkbox>
+                    </FormItem>
+
+                    {checkbox1 && (
+                        <>
+                            <FormItem top="Количество человек">
+                                <Slider step={1} min={0} max={50} value={Number(value2)} onChange={setValue2} />
+                            </FormItem>
+                            <FormItem>
+                                <Input
+                                    min={0}
+                                    value={String(value2)}
+                                    onChange={(e) => setValue2(e.target.value)}
+                                    type="number"
+                                />
+                            </FormItem>
+                        </>
+                    )}
+                    <FormItem
+                        top="Начало записи в очередь"
+                    >
+                        <div style={{ display: 'flex' }}>
+                            <LocaleProvider value={locale}>
+                                <DateInput
+                                    value={startDate}
+                                    onChange={setStartDate}
+                                    enableTime={enableTime}
+                                    disablePast={disablePast}
+                                    disableFuture={disableFuture}
+                                    closeOnChange={closeOnChange}
+                                    disablePickers={disablePickers}
+                                    showNeighboringMonth={showNeighboringMonth}
+                                    disableCalendar={disableCalendar}
+                                />
+                            </LocaleProvider>
+                        </div>
+                    </FormItem>
+                    <FormItem
+                        top="Конец записи в очередь"
+                        bottom={<span style={{ color: 'red' }}>{dateError}</span>}
+                    >
+                        <div style={{ display: 'flex' }}>
+                            <LocaleProvider value={locale}>
+                                <DateInput
+                                    value={endDate}
+                                    onChange={setEndDate}
+                                    enableTime={enableTime}
+                                    disablePast={disablePast}
+                                    disableFuture={disableFuture}
+                                    closeOnChange={closeOnChange}
+                                    disablePickers={disablePickers}
+                                    showNeighboringMonth={showNeighboringMonth}
+                                    disableCalendar={disableCalendar}
+                                />
+                            </LocaleProvider>
+                        </div>
+                    </FormItem>
+                </>
+            )}
+
+            {activeTab === 'дополнительные' && (
+                <>
+                    <FormItem top="Дополнительные настройки">
+                        <Checkbox checked={checkbox1} onChange={() => setCheckbox1(!checkbox1)}>
+                            Автоматически перемешать очередь
+                        </Checkbox>
+                        <Checkbox checked={checkbox2} onChange={() => setCheckbox2(!checkbox2)}>
+                            Режим выбора места в очереди
+                        </Checkbox>
+                        <Checkbox checked={checkbox3} onChange={() => setCheckbox3(!checkbox3)}>
+                            Режим заявок на очередь
+                        </Checkbox>
+                    </FormItem>
+
+                    <FormItem top="Время на человека">
+                        <Input placeholder="Введите время" />
+                    </FormItem>
+                </>
+            )}
+
+            <Group>
+                <Button
+                    mode="commerce"
+                    size="xl"
+                    style={{
+                        marginLeft: 16,
+                        marginRight: 16,
+                        marginTop: 12,
+                        marginBottom: 12,
+                        background: '#2688EB',
+                        borderRadius: 12,
+                        color: '#FFFFFF',
+                        padding: '12px 16px',
+                        width: 'calc(100% - 32px)',
+                        boxShadow: 'none',
+                        transition: 'box-shadow 0.15s ease-in-out',
+                    }}
+                    hoverMode="opacity"
+                    activeMode="opacity"
+                    onClick={handleSubmit}
+                >
+                    Создать очередь
+                </Button>
+            </Group>
+        </ModalPage>
+    );
+};
+
+export default QueueModal;
